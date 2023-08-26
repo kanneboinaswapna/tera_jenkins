5217f4681bf18e8df046f3725e908e4a8e0ce1c3
swapna <swapna@kanneboinaswapna741@gmail.com>
2023-08-26 13:19:56 +0530

    terraform resource deploy in jenkins5

--- inputs.tf
+++ inputs.tf
@@ -4,7 +4,7 @@ variable "vpc" {
 }
 variable "subnet_cidr" {
   type = list(string)
-  default = ["10.0.0.0/24", "10.0.0.1/24", "10.0.0.2/24"]
+  default = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "subnet_name" {
